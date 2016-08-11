616a8ab20319fbaf6f7225723506e516552ea309
l46kok <2355949+l46kok@users.noreply.github.com>
2016-08-10 18:19:31 -0600

    Initial Commit

---
+++ RobotProject/irsensor.h
@@ -0,0 +1,20 @@
+﻿/*
+ * irsensor.h
+ *
+ * Created: 2016-08-09 오전 1:47:42
+ *  Author: Sokwhan
+ */ 
+
+
+#ifndef IRSENSOR_H_
+#define IRSENSOR_H_
+
+#include "hardware.h"
+
+typedef enum e_sensor {	SENSOR_LEFT, SENSOR_CENTER, SENSOR_RIGHT, SENSOR_MINE } SensorType;
+
+void initADC(void);
+float getSensorValue(SensorType sensor);
+
+
+#endif /* IRSENSOR_H_ */
